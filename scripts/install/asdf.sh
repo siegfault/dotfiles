dd7f199b6d54a98c18c051d55187f01f73dd87ec
Michael Siegfried <michael@procore.com>
2020-08-18 14:26:15 -0700

    Fixes asdf installs
    * Need to include is_installed before I can use it
    * Need to give a version to install - using latest version

--- scripts/install/asdf.sh
+++ scripts/install/asdf.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+source "scripts/install/is_installed.sh"
+
 function asdf_install {
   if ! is_installed $1 ; then
     asdf plugin add $1
-    asdf install $1
+    asdf install $1 $(asdf latest $1)
   fi
 }
 
