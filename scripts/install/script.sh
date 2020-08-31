715694a4c75bc763f1c6cb453b89c0201c16a67c
Michael Siegfried <michael@procore.com>
2020-08-31 15:37:59 -0700

    Adds aws-iam-authenticator

--- scripts/install/script.sh
+++ scripts/install/script.sh
@@ -8,3 +8,12 @@ if [ ! -d /home/siggy/.oh-my-zsh ]; then
 else
   echo "Already installed: OhMyZsh"
 fi
+
+AWS_IAM_AUTH_LOC=/usr/bin/aws-iam-authenticator
+if [ ! -f $AWS_IAM_AUTH_LOC ]; then
+  echo "Installing: aws-iam-authenticator"
+  sudo curl -o $AWS_IAM_AUTH_LOC https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.9/2020-08-04/bin/linux/amd64/aws-iam-authenticator
+  sudo chmod +x $AWS_IAM_AUTH_LOC
+else
+  echo "Already installed: aws-iam-authenticator"
+fi
