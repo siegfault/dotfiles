74d3902c06fd3d3be96667e122cddefdda59adb8
Michael Siegfried <michael@procore.com>
2020-10-17 18:48:04 -0700

    Skips rust prompt

--- scripts/install/cargo.sh
+++ scripts/install/cargo.sh
@@ -3,7 +3,7 @@
 source "scripts/install/is_installed.sh"
 
 if ! is_installed cargo ; then
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh --profile default -qy
 fi
 
 function cargo_install {
