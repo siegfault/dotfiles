0bef1698b1b5abcd338609060933e74d95ed80c0
Michael Siegfried <michaeldsiegfried@gmail.com>
2022-10-15 18:34:09 -0700

    Adds SHAs function back
    I've already found a use for it. The following revert...

--- functions.sh
+++ functions.sh
@@ -30,6 +30,16 @@ _gb() {
   sed 's#^origin/##'
 }
 
+# SHAs
+_gh() {
+  is_in_git_repo || return
+  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
+  fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+    --header 'Press CTRL-S to toggle sort' \
+    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always' |
+  grep -o "[a-f0-9]\{7,\}"
+}
+
 # Stashes
 _gp() {
   is_in_git_repo || return
