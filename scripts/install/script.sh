00805857ea02870eeda87ed9a1e0eac513b2e3d7
Michael Siegfried <michaeldsiegfried@gmail.com>
2023-08-27 11:39:50 -0600

    Install zplug automatically

--- scripts/install/script.sh
+++ scripts/install/script.sh
@@ -5,3 +5,7 @@ source "scripts/install/is_installed.sh"
 if ! is_installed npm ; then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 fi
+
+if ! is_installed zplug ; then
+  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+fi
